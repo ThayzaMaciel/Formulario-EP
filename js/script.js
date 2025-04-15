5ae7622bd6da45b69c4ebeb0d5adf59503629ca5
Thayza <macielthayzale@gmail.com>
2025-04-15 10:52:26 -0300

    Adicionado logica de salas e outro monte de coisa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-
 const form = document.getElementById("formInfor")
+
+
+let alunoSala = {}
+
+function atualizarContadores() {
+    for (const sala in alunoSala) {
+        const contador = document.getElementById(`contador-sala-${sala.split(" ")[1]}`);
+        if (contador) {
+            contador.innerText = alunoSala[sala].length;
+        }
+    }
+}
 
     form.addEventListener("submit", (e)=> {
         e.preventDefault()
@@ -9,6 +20,23 @@ const form = document.getElementById("formInfor")
         formData.forEach((value, key)=> {
             data[key] = value
         })
-
         console.log(data)
+
+        const aluno = data.name
+        const sala = data.sala
+
+       
+        if(!alunoSala[sala]) {
+            alunoSala[sala] = [];
+        }
+
+        if(alunoSala[sala].length < 35) {
+            alunoSala[sala].push(aluno);
+            MessageSucess(`Aluno(a) ${aluno} cadastrado na sala com sucesso na sala: ${sala}`)
+        } else {
+            MessageErro(`Sala cheia! impossivel cadastrar aluno(a) ${aluno} na sala ${sala}`)
+        }
     })
+
+    
+
