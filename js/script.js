eed4104a330fe53717b1680c2f6f3702c670cb54
Thayza <macielthayzale@gmail.com>
2025-04-15 15:50:07 -0300

    Projeto concluido

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function atualizarContadores() {
     }
 }
 
-    form.addEventListener("submit", (e)=> {
+    form.addEventListener("submit", async (e)=> {
         e.preventDefault()
         const formData = new FormData(form)
 
@@ -21,6 +21,22 @@ function atualizarContadores() {
             data[key] = value
         })
         console.log(data)
+
+        try {
+            const response = await fetch("https://script.google.com/macros/s/AKfycbwC-x2gCBe5uXB-tALBsyw0WBTV0rFWRYjZw_WIBdF-TYx7fOt9wLWI5uM6ZhbdCgJD/exec", {
+                method: "POST",
+                mode: "no-cors",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            });
+
+            const result = await response.json();
+
+        } catch (err) {
+            MediaError("Erro de conexão")
+        }
 
         const aluno = data.name
         const sala = data.sala
